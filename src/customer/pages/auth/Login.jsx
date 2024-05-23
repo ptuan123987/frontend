2bf986aa42b63381412a884361955d4cb25a579c
Phan Van Tuan <ptuan123987@gmail.com>
2024-05-23 09:40:15 +0700

    update social login

--- src/customer/pages/auth/Login.jsx
+++ src/customer/pages/auth/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
   async function redirectToGoogleLogin() {
     const response = await axios.get(API_URL + "api/login/google", {
       params: {
-        redirect_url: "https://study.salyr.online/callback/google",
+        redirect_url: "http://13.229.208.74/callback/google",
       },
     });
     console.log(response);
@@ -41,7 +41,7 @@ const Login = () => {
   async function redirectToGithubLogin() {
     const response = await axios.get( API_URL + "api/login/github", {
       params: {
-        redirect_url: "https://study.salyr.online/callback/github",
+        redirect_url: "http://13.229.208.74/callback/github",
       },
     });
     console.log(response);
